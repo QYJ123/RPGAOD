b427af584bcbd646f0b779cdc173261c2d0e1f76
QYJ123 <46787440+QYJ123@users.noreply.github.com>
2023-05-18 09:20:01 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,10 @@
+#./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dior.py 2 --seed 0
+#./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dota10.py 2 --seed 0
+./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dota10_2.py 2 --seed 0
+./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dota10_3.py 2 --seed 0
+./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dota10_4.py 2 --seed 0
+
+
+./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dior.py 2 --seed 0
+./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dior_3.py 2 --seed 0
+./tools/dist_train.sh ./configs/obb/my_improve/SR2_ORCNN_r50_dior_4.py 2 --seed 0
